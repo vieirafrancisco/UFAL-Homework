1a7f9e130fa1dfcf0595f08e44fd65d3085603c8
Francisco Neto <fvcneto.master98@gmail.com>
2020-01-24 11:30:56 -0300

    setup bd django project

---
+++ bd2/relational_to_object/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lica/Documents/Francisco/github/UFAL-Homework/bd2/relational_to_object/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
