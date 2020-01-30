ed7b5f462a56d593420c29e0bba95a98caca1312
Francisco Neto <fvcneto.master98@gmail.com>
2020-01-30 13:34:49 -0300

    create other models in the forum database

--- bd2/relational_to_object/myproject/forum/models.py
+++ bd2/relational_to_object/myproject/forum/models.py
@@ -1,6 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    image_url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    manager = models.ForeignKey(User, on_delete=models.CASCADE)
+    # tag - ManyToManyField
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_lenght=200)
+    state = models.BooleanField(null=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_author_name(self):
+        return manager.name
+
 class Post(models.Model):
-    name = models.CharField(max_length=200)
-    content = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+
+# create Tag model
